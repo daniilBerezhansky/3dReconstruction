1d56622ae7ff769127aeb74049dbc551b27507a7
Tania KLueva <39909234+tania-klueva@users.noreply.github.com>
2019-05-22 09:24:50 +0300

    Post creation (#2)
    * created form and post method for Post model
    
    * created form on view
    
    * fixings
    
    * fixings

--- django_project/blog/forms.py
+++ django_project/blog/forms.py
@@ -1,9 +1,16 @@
 from django import forms
 
 from .models import Photo
+from .models import Post
 
 
 class PhotoForm(forms.ModelForm):
     class Meta:
         model = Photo
-        fields = ('file', )
+        fields = ('file',)
+
+
+class PostForm(forms.ModelForm):
+    class Meta:
+        model = Post
+        fields = ['title', 'content']
--- django_project/blog/views.py
+++ django_project/blog/views.py
@@ -1,10 +1,11 @@
 from django.core.files.storage import FileSystemStorage
-from django.http import JsonResponse
-from django.shortcuts import render
+from django.http import JsonResponse, HttpResponseRedirect
+from django.shortcuts import render, redirect
 from django.views import View
 from django.views.generic import CreateView
 
 from blog.forms import PhotoForm
+from blog.forms import PostForm
 # from django_project import settings
 from .models import Post, Photo
 
@@ -36,7 +37,16 @@ class Reconstruction(View):
             data = {'is_valid': False}
         return JsonResponse(data)
 
+
 class Posts(View):
     def get(self, request):
         posts_list = Post.objects.all()
         return render(self.request, 'blog/posts.html', {'posts': posts_list})
+
+    def post(self, request):
+        form = PostForm(self.request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.save()
+        return redirect('posts')
